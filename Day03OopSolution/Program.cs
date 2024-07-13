3ac438d938b03d256bb928bb2d16647bdbc210d7
ibrahymMahmoud <ibrahymmahmoud123@gmail.com>
2024-07-13 12:25:44 +0300

    Add project files.

---
+++ Day03OopSolution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day03OopSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
